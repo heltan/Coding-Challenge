b16ade1ebc5a1de5c9a69f1aeb475266fc10d7ba
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-12-19 14:48:26 -0800

    Add whiteboarding notes and begin code

---
+++ challenge.py
@@ -0,0 +1,19 @@
+#Write a python program that iterates through all the numbers from 2 to 144 (inclusive). 
+#If a number is a perfect square, print 'fizz'. If the number is a prime number, print 'buzz'. 
+# If the the number is a perfect square and its square-root is a prime number, print 'fizzbuzz'
+
+#whiteboarding below
+#Output: none, just print strings based on conditions
+#Input: none
+#Constraints: iterate through all numbers from 2 to 144, inclusive
+#Edge Case: none
+import math
+def fizzbuzz():
+    for x in range(2,145):
+
+        #if number is perfect square
+        if math.sqrt(x).is_integer() == True:
+            print(x)
+            print('fizz')
+
+fizzbuzz()
