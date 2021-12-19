0685e13cc43258d6b64b67c2aad92f60a40c2c58
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-12-19 15:12:38 -0800

    Complete fizzbuzz and fixed the issue of trying to run range with a float number instead of an integer

--- challenge.py
+++ challenge.py
@@ -6,7 +6,8 @@
 #Output: none, just print strings based on conditions
 #Input: none
 #Constraints: iterate through all numbers from 2 to 144, inclusive
-#Edge Case: if the number is a perfect square should it print 'fizz' and then 'fizzbuzz'? or just print 'fizzbuzz'?
+#Edge Case: if the number is a perfect square and then it's square
+# root is also a prime number, should it print 'fizz' and then 'fizzbuzz'? or just print 'fizzbuzz'?
             
 import math
 
@@ -14,8 +15,8 @@ import math
 def isPrime(num):
     isItPrime = True
     #divide number in half and loop to test
-    #if number is less than 3, just return false
-    #num will never be one because of the range defined in fizzbuzz
+    #if number is less than 3, just return True
+    #num will never be one (which is not a prime number) because of the range defined in fizzbuzz
     if num < 3:
         return True
     else:
@@ -28,10 +29,11 @@ def isPrime(num):
 
 def fizzbuzz():
     for x in range(2,145):
-        isPerfectSquare = False
+        xisPerfectSquare = False
+        print(x)
         #if number is perfect square, print 'fizz'
         if math.sqrt(x).is_integer() == True:
-            isPerfectSquare = True
+            xisPerfectSquare = True
             print('fizz')
         #check if number is prime num, then print 'buzz'
         #prime numbers are numbers greater than 1 where it can only divide into 1 and itself
@@ -40,4 +42,9 @@ def fizzbuzz():
 
         #check if number is a perfect square AND squre root is a prime number, print 'fizzbuzz'
         #check if the square root is a number and not a float
-        if isPerfectSquare == True and isPrime(math.sqrt(x))
+        squareRootOfX = math.sqrt(x)
+        if squareRootOfX.is_integer() == True:
+            #now check and don't forget to convert to int to check prime status
+            if xisPerfectSquare == True and isPrime(int(squareRootOfX)) == True:
+                print('fizzbuzz')
+fizzbuzz()
